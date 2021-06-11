73fdb9603c566ac36fe125b26ba5ba1bf1bf684b
Sergii Siryi <sergii.siryi97@gmail.com>
2021-06-11 21:43:38 +0200

    add invoice template to project

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
+const Twig = require('twig');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/html-to-pdf', (request, response) => {
-    const html = request.body.html;
+app.post('/v1/invoice/generate', (request, response) => {
+    const twigData = request.body.data;
 
-    (async () => {
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    Twig.renderFile('./invoice.html.twig', {...twigData}, (err, html) => {
+        (async () => {
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
+            const page = await browser.newPage();
+            await page.setContent(html);
+            const pdf = await page.pdf({format: 'A4'});
+            await browser.close();
+
+            return pdf;
+        })().then(pdf => {
+            response.set({'Content-Type': 'application/pdf'});
+            response.send(pdf);
+        }).catch(error => {
+            response.status(500);
+            response.send(error);
         });
-        const page = await browser.newPage();
-        await page.setContent(html);
-        const pdf = await page.pdf({format: 'A4'});
-        await browser.close();
-
-        return pdf;
-    })().then(pdf => {
-        response.set({'Content-Type': 'application/pdf'});
-        response.send(pdf);
-    }).catch(error => {
-        response.status(500);
-        console.log(error);
-        response.send(error);
     });
 })
 
